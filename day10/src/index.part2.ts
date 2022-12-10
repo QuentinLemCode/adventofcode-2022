9451712ddec1047ff8d30de167c3bf6485b854d3
QuentinLemCode <quentinlemcode@gmail.com>
2022-12-10 11:48:56 +0100

    day 10 part 2

--- day10/src/index.part2.ts
+++ day10/src/index.part2.ts
@@ -1,12 +1,48 @@
 import { ReadFile } from './fileread';
 
 const file = new ReadFile('input.txt');
+const commands: { type: string; value: number | undefined }[] = [];
 
 function onEachLine(line: string) {
-  console.log(line);
+  const [type, value] = line.split(' ');
+  commands.push({ type, value: Number(value) });
 }
 
 async function process() {
   await file.applyFunction(onEachLine);
+  const rows: string[] = [];
+
+  let cycles = 0;
+  let sp = 0;
+  let w = false;
+  let x = 1;
+  let end = false;
+  let row: string[] = [];
+  let spriteStart = 0;
+  let spriteStop = 2;
+  while (!end) {
+    const column = cycles % 40;
+    if (column === 0) console.log(row.join(''));
+    row[column] = column >= spriteStart && column <= spriteStop ? '#' : '.';
+    cycles++;
+    const command = commands[sp];
+    if (command.type === 'noop') {
+      sp++;
+    } else if (command.type === 'addx') {
+      if (w === false) {
+        w = true;
+      } else {
+        sp++;
+        x += command.value!;
+        spriteStart = x - 1;
+        spriteStop = x + 1;
+        w = false;
+      }
+    }
+    if (sp >= commands.length - 1) {
+      end = true;
+    }
+  }
+  console.log(row.join(''));
 }
 process().then();
