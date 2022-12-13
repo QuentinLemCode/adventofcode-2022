a9727096dba3563b6fd891109fcbb638bcf1b967
QuentinLemCode <quentinlemcode@gmail.com>
2022-12-13 08:06:41 +0100

    day 13 part 1

--- day13/src/index.part1.ts
+++ day13/src/index.part1.ts
@@ -1,12 +1,48 @@
-import { ReadFile } from './fileread';
+import { readFileSync } from 'fs';
+type Packet = number | Packet[];
 
-const file = new ReadFile('input.txt');
+const inputPairs = readFileSync('input.txt')
+  .toString()
+  .split('\n\n')
+  .map(pairs => pairs.split('\n'));
+const input: [Packet[], Packet[]][] = inputPairs.map(inputPair => [
+  eval(inputPair[0]),
+  eval(inputPair[1]),
+]);
 
-function onEachLine(line: string) {
-  console.log(line);
+function compare(left: Packet[], right: Packet[]): number {
+  for (let i = 0, len = Math.max(left.length, right.length); i < len; i++) {
+    if (left.length <= i) return -1;
+    if (right.length <= i) return 1;
+    const leftValue = left[i];
+    const rightValue = right[i];
+    if (typeof leftValue === 'number' && typeof rightValue === 'number') {
+      if (leftValue !== rightValue) {
+        return leftValue < rightValue ? -1 : 1;
+      }
+    } else {
+      const leftArr = Array.isArray(leftValue) ? leftValue : [leftValue];
+      const rightArr = Array.isArray(rightValue) ? rightValue : [rightValue];
+      const result = compare(leftArr, rightArr);
+      if (result !== 0) {
+        return result;
+      }
+    }
+  }
+  return 0;
 }
 
-async function process() {
-  await file.applyFunction(onEachLine);
-}
-process().then();
+const results = input.map(([left, right]) => {
+  const comparison = compare(left, right);
+  if (comparison === -1) return true;
+  return false;
+});
+
+let count = 0;
+results.forEach((bool, index) => {
+  if (bool) {
+    count += index + 1;
+  }
+});
+
+console.log(count);
